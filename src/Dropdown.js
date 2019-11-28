9dd00d708dba165f6a26c89ee2b0e6d8ed8c7b61
janovich <jan_postema1@live.nl>
2019-11-28 12:44:58 +0100

    bootstrap css

---
+++ src/Dropdown.js
@@ -0,0 +1,16 @@
+import React from 'react';
+ 
+const Dropdown = () => {
+    return (
+        <div>
+            <h1>Dropdown</h1>
+            <row>
+                <div className='col-12 bg-primary blue' >
+                        <h1>DROPDOWN</h1>
+                </div>
+            </row>
+        </div>
+    );
+};
+ 
+export default Dropdown;
