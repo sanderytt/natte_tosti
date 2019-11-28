d80a2849127c8fe28e8668cd245867098473cc8e
Sander van Rijsoort <sander.vanrijsoort@incentro.com>
2019-11-28 11:12:33 +0100

    Masterrr

--- src/App.js
+++ src/App.js
@@ -1,26 +1,30 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const API = "https://developers.zomato.com/api/v2.1/cities?q=Santander"
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      cityId: 0
+    }
+  }
+
+  componentDidMount() {
+    fetch(API)
+    .then(response => response.json())
+    .then(data => this.setState({ cityId: data.cityId }));
+  }
+
+  render() {
+    return (
+      <div>
+        <h1>Restaurant + route finder in Santander</h1>
+      </div>
+    );
+  }
 }
 
 export default App;
